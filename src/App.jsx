cc0e4932c4318b73f5d7c33b71cb34242d82cddd
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-11 04:48:26 +0600

    Added Static  Square Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,37 @@
+function Square({ value }) {
+  function handleClick() {
+    console.log("Clicked");
+  }
+
+  return (
+    <button
+      className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
   return (
     <>
       <div>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
 
       <div>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
 
       <div>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
-        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
-          X
-        </button>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
