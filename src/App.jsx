aee812842606b8162e99025dfe56cc14e5f18ed8
md-ahmodullah <contactme.ahmod@gmail.com>
2024-10-11 04:28:23 +0600

    Created Board Component With 9 Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
-export default function App() {
-  return;
+export default function Board() {
+  return (
+    <>
+      <div>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+      </div>
+
+      <div>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+      </div>
+
+      <div>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+        <button className="bg-white w-12 h-12 m-1 border border-gray-400 leading-9">
+          X
+        </button>
+      </div>
+    </>
+  );
 }
